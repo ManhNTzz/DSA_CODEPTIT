61f2e609bb143340968133dc322422ccf8c48fa7
Mạnh Nguyễn <toilamanh16@gmail.com>
2026-08-31 09:19:41 +0700

    Update DSA01008 - XÂU NHỊ PHÂN CÓ K BIT 1.cpp

--- DSA01008 - XÂU NHỊ PHÂN CÓ K BIT 1.cpp
+++ DSA01008 - XÂU NHỊ PHÂN CÓ K BIT 1.cpp
@@ -19,19 +19,14 @@ const int INF = 1e9;
 
 int n, k;
 int a[105];
-void Try(int i)
-{
-	FOR(j, 0, 1)
-	{
+void Try(int i){
+	FOR(j, 0, 1){
 		a[i] = j;
-		if (i == n)
-		{
+		if (i == n){
 			int dem = 0;
 			FOR(i, 1, n)
-			if (a[i])
-				dem++;
-			if (dem == k)
-			{
+			if (a[i]) dem++;
+			if (dem == k){
 				FOR(i, 1, n)
 				cout << a[i];
 				cout << endl;
@@ -41,15 +36,13 @@ void Try(int i)
 			Try(i + 1);
 	}
 }
-int main()
-{
+int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	int T;
 	cin >> T;
-	while (T--)
-	{
+	while (T--){
 		cin >> n >> k;
 		Try(1);
 	}
