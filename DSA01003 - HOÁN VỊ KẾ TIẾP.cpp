a61fb44e5c2486fa2781980190abbe213d51ea4e
Mạnh Nguyễn <toilamanh16@gmail.com>
2026-08-31 09:09:10 +0700

    Update DSA01003 - HOÁN VỊ KẾ TIẾP.cpp

--- DSA01003 - HOÁN VỊ KẾ TIẾP.cpp
+++ DSA01003 - HOÁN VỊ KẾ TIẾP.cpp
@@ -13,53 +13,41 @@ using namespace std;
 
 // Cre by ManhNTzz
 
-void testCase()
-{
+void testCase(){
 	int n;
 	cin >> n;
 	vector<int> a(n + 1);
-	for (int i = 1; i <= n; ++i)
-	{
+	for (int i = 1; i <= n; ++i){
 		cin >> a[i];
 	}
 
 	int i = n - 1;
-	while (i >= 1 && a[i] >= a[i + 1])
-		i--;
-	if (i == 0)
-	{
-		for (int j = 1; j <= n; ++j)
-		{
+	while (i >= 1 && a[i] >= a[i + 1]) i--;
+	if (i == 0){
+		for (int j = 1; j <= n; ++j){
 			a[j] = j;
 		}
 	}
-	else
-	{
-		for (int j = n; j > i; --j)
-		{
-			if (a[j] > a[i])
-			{
+	else{
+		for (int j = n; j > i; --j){
+			if (a[j] > a[i]){
 				swap(a[i], a[j]);
 				break;
 			}
 		}
 	}
-	for (int i = 1; i <= n; ++i)
-	{
+	for (int i = 1; i <= n; ++i){
 		cout << a[i] << " ";
 	}
 }
 
-int main()
-{
+int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
 	int T = 1;
 	cin >> T;
-	while (T--)
-	{
+	while (T--){
 		testCase();
 		cout << "\n";
 	}
