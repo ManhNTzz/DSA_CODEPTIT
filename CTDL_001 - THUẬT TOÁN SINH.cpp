499ca2474030e261e5a9755b272ec163274db6fc
Mạnh Nguyễn <toilamanh16@gmail.com>
2026-08-31 09:12:22 +0700

    Update CTDL_001 - THUẬT TOÁN SINH.cpp

--- CTDL_001 - THUẬT TOÁN SINH.cpp
+++ CTDL_001 - THUẬT TOÁN SINH.cpp
@@ -19,24 +19,18 @@ const int INF = 1e9;
 
 int n;
 int a[105];
-void Try(int i)
-{
-	FOR(j, 0, 1)
-	{
+void Try(int i){
+	FOR(j, 0, 1){
 		a[i] = j;
-		if (i == n)
-		{
+		if (i == n){
 			int ok = 0;
-			for (int i = 1; i <= n / 2; i++)
-			{
-				if (a[i] != a[n - i + 1])
-				{
+			for (int i = 1; i <= n / 2; i++){
+				if (a[i] != a[n - i + 1]){
 					ok = 1;
 					break;
 				}
 			}
-			if (!ok)
-			{
+			if (!ok){
 				FOR(i, 1, n)
 				cout << a[i] << " ";
 				cout << endl;
@@ -46,8 +40,7 @@ void Try(int i)
 			Try(i + 1);
 	}
 }
-int main()
-{
+int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
