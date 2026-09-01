776ad49bae7d86d1f5a8886a5f8512829adf57e8
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-01 20:46:08 +0700

    Code DSA

--- DSA01011 - HOÁN VỊ TIẾP THEO CỦA CHUỖI SỐ.cpp
+++ DSA01011 - HOÁN VỊ TIẾP THEO CỦA CHUỖI SỐ.cpp
@@ -15,31 +15,26 @@ const long long MOD = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     int T;
     cin >> T;
-    FOR(c, 1, T)
-    {
+    FOR(c, 1, T){
         int x;
         string s;
         cin >> x >> s;
         cout << x << " ";
         int i = s.len - 2;
-        while (i >= 0 && s[i] >= s[i + 1])
-            i--;
-        if (i < 0)
-        {
+        while (i >= 0 && s[i] >= s[i + 1]) i--;
+        if (i < 0){
             cout << "BIGGEST" << endl;
             continue;
         }
         int j;
         for (j = s.len - 1; j > i; --j)
-            if (s[j] > s[i])
-            {
+            if (s[j] > s[i]) {
                 swap(s[j], s[i]);
                 break;
             }
