7c2e7f4ff1932ad931df0c5a0ca43ae85b33f18f
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-10 12:10:19 +0700

    Code DSA

--- DSA02001 - DÃY SỐ 1.cpp
+++ DSA02001 - DÃY SỐ 1.cpp
@@ -17,26 +17,22 @@ const int INF = 1e9;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	int T;
 	cin >> T;
-	while (T--)
-	{
+	while (T--) {
 		int n;
 		cin >> n;
 		vector<int> a;
 		a.resize(n + 1);
 		FOR(i, 1, n)
 		cin >> a[i];
-		while (n > 0)
-		{
+		while (n > 0) {
 			cout << "[";
-			FOR(i, 1, n)
-			{
+			FOR(i, 1, n){
 				cout << a[i];
 				if (i != n)
 					cout << " ";
