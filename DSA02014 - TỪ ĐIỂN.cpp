4060fe684ad513b88b714e1736ea8f323e4289d1
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-10 12:19:57 +0700

    Code DSA

--- DSA02014 - TỪ ĐIỂN.cpp
+++ DSA02014 - TỪ ĐIỂN.cpp
@@ -13,20 +13,16 @@ bool vis[3][3] = {};
 int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
 int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-void DFS(int x, int y, string s, unordered_map<string, string> &mp)
-{
-	for (string t : a)
-	{
+void DFS(int x, int y, string s, unordered_map<string, string> &mp) {
+	for (string t : a) {
 		if (s == t)
 			res.push_back(s);
 	}
 
-	for (int i = 0; i < 8; i++)
-	{
+	for (int i = 0; i < 8; i++) {
 		int x1 = x + dx[i];
 		int y1 = y + dy[i];
-		if (x1 >= 0 && x1 < n && y1 >= 0 && y1 < m && !vis[x1][y1])
-		{
+		if (x1 >= 0 && x1 < n && y1 >= 0 && y1 < m && !vis[x1][y1]) {
 			vis[x1][y1] = true;
 			DFS(x1, y1, s + string(1, ch[x1][y1]), mp);
 			vis[x1][y1] = false;
@@ -34,35 +30,29 @@ void DFS(int x, int y, string s, unordered_map<string, string> &mp)
 	}
 }
 
-int main()
-{
+int main() {
 	int t;
 	cin >> t;
-	while (t--)
-	{
+	while (t--) {
 		a.clear();
 		res.clear();
 
 		cin >> k >> n >> m;
 
 		unordered_map<string, string> mp;
-		for (int i = 0; i < k; i++)
-		{
+		for (int i = 0; i < k; i++) {
 			string s;
 			cin >> s;
 			a.push_back(s);
 		}
 
-		for (int i = 0; i < n; i++)
-		{
+		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++)
 				cin >> ch[i][j];
 		}
 
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
 				memset(vis, false, sizeof(vis));
 				vis[i][j] = true;
 				DFS(i, j, string(1, ch[i][j]), mp);
@@ -71,8 +61,7 @@ int main()
 
 		if (res.empty())
 			cout << "-1" << endl;
-		else
-		{
+		else {
 			for (string s : res)
 				cout << s << " ";
 			cout << endl;
