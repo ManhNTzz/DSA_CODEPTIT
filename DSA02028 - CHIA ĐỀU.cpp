eaa7642997c70d8333e6028a7941d133b5a67383
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-14 13:48:45 +0700

    Code DSA

--- DSA02028 - CHIA ĐỀU.cpp
+++ DSA02028 - CHIA ĐỀU.cpp
@@ -5,37 +5,31 @@ using namespace std;
 
 // Cre by ManhNTzz
 
-void Try(int i, int k, int sum, int target, vector<int> &a, int &cnt)
-{
-	if (i == a.size() && k == 0)
-	{
+void Try(int i, int k, int sum, int target, vector<int> &a, int &cnt) {
+	if (i == a.size() && k == 0) {
 		cnt++;
 		return;
 	}
 
-	for (int j = i; j < a.size(); j++)
-	{
+	for (int j = i; j < a.size(); j++) {
 		sum += a[j];
 		if (sum == target)
 			Try(j + 1, k - 1, 0, target, a, cnt);
 	}
 }
 
-int main()
-{
+int main() {
 	int n, k;
 	cin >> n >> k;
 	vector<int> a;
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		int x;
 		cin >> x;
 		a.push_back(x);
 	}
 
 	int sum = accumulate(a.begin(), a.end(), 0);
-	if (sum % k != 0)
-	{
+	if (sum % k != 0) {
 		cout << "0" << endl;
 		return 0;
 	}
