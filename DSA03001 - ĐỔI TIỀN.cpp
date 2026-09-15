7dc84034662f2eea5beb118f68285940eba29597
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-15 13:25:43 +0700

    Code DSA

--- DSA03001 - ĐỔI TIỀN.cpp
+++ DSA03001 - ĐỔI TIỀN.cpp
@@ -13,21 +13,18 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	int T;
 	cin >> T;
-	while (T--)
-	{
+	while (T--) {
 		int a[10] = {1, 2, 5, 10, 20, 50, 100, 200, 500, 1000};
 		int n;
 		cin >> n;
 		ll ans = 0;
-		FOD(i, 0, 9)
-		{
+		FOD(i, 0, 9) {
 			ans += n / a[i];
 			n %= a[i];
 		}
