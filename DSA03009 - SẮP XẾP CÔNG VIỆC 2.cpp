6231196e41d67489ba53ec3385f19b114c64a14d
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-16 09:21:46 +0700

    Code DSA

--- DSA03009 - SẮP XẾP CÔNG VIỆC 2.cpp
+++ DSA03009 - SẮP XẾP CÔNG VIỆC 2.cpp
@@ -13,38 +13,31 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-bool cmp(pair<int, int> a, pair<int, int> b)
-{
+bool cmp(pair<int, int> a, pair<int, int> b) {
 	if (a.se == b.se)
 		return a.fi > b.fi;
 	return a.se > b.se;
 }
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	int T;
 	cin >> T;
-	while (T--)
-	{
+	while (T--) {
 		int n, x, maxx = 0;
 		cin >> n;
 		vector<pair<int, int>> a(n);
-		REP(i, n)
-		{
+		REP(i, n) {
 			cin >> x >> a[i].fi >> a[i].se;
 			maxx = max(maxx, a[i].fi);
 		}
 		sort(ALL(a), cmp);
 		vector<bool> dd(maxx + 1, 0);
 		pair<int, int> ans = {0, 0};
-		REP(i, n)
-		{
-			for (int j = a[i].fi; j >= 1; --j)
-			{
-				if (!dd[j])
-				{
+		REP(i, n) {
+			for (int j = a[i].fi; j >= 1; --j) {
+				if (!dd[j]) {
 					dd[j] = 1;
 					ans.fi++;
 					ans.se += a[i].se;
