34333a9e306edec51c2686b374209e86c6842848
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-16 09:23:00 +0700

    Code DSA

--- DSA03012 - SẮP ĐẶT XÂU KÝ TỰ 1.cpp
+++ DSA03012 - SẮP ĐẶT XÂU KÝ TỰ 1.cpp
@@ -3,35 +3,29 @@ using namespace std;
 
 // Cre by ManhNTzz
 
-void process()
-{
+void process() {
 	string s;
 	cin >> s;
 	int n = s.length();
 	vector<int> a(26, 0);
-	for (int i = 0; i < s.length(); ++i)
-	{
+	for (int i = 0; i < s.length(); ++i) {
 		a[s[i] - 'a']++;
 	}
 	int ok = 1;
-	for (int i = 0; i < 26; ++i)
-	{
-		if (a[i] > ((n + 1) / 2))
-		{
+	for (int i = 0; i < 26; ++i) {
+		if (a[i] > ((n + 1) / 2)) {
 			ok = -1;
 			break;
 		}
 	}
 	cout << ok << endl;
 }
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int T;
 	cin >> T;
-	while (T--)
-	{
+	while (T--) {
 		process();
 	}
 	return 0;
