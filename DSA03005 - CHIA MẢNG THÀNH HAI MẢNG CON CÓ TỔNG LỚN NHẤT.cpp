377784589f45a1955d94ed7e2bb2fd86b6dd511c
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-16 09:19:44 +0700

    Code DSA

--- DSA03005 - CHIA MẢNG THÀNH HAI MẢNG CON CÓ TỔNG LỚN NHẤT.cpp
+++ DSA03005 - CHIA MẢNG THÀNH HAI MẢNG CON CÓ TỔNG LỚN NHẤT.cpp
@@ -15,15 +15,13 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	int T;
 	cin >> T;
-	while (T--)
-	{
+	while (T--) {
 		int n, k;
 		cin >> n >> k;
 		k = min(k, n - k);
