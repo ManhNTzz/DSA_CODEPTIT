d8b9fd93811b09df0f9679d57df18258e4480cba
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-17 07:26:30 +0700

    Code DSA

--- DSA03016 - SỐ NHỎ NHẤT.cpp
+++ DSA03016 - SỐ NHỎ NHẤT.cpp
@@ -15,26 +15,22 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	int T;
 	cin >> T;
-	while (T--)
-	{
+	while (T--) {
 		int s, d;
 		cin >> s >> d;
-		if (s > 9 * d)
-		{
+		if (s > 9 * d) {
 			cout << -1 << endl;
 			continue;
 		}
 		int a[d] = {1};
 		s--;
-		for (int i = d - 1; i >= 0; --i)
-		{
+		for (int i = d - 1; i >= 0; --i) {
 			int x = min(s, 9 - a[i]);
 			a[i] += x;
 			s -= x;
