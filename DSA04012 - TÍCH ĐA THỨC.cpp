9f2fb05337c0e42e79748453a1c267c32c7406f6
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-18 08:46:03 +0700

    Code DSA

--- DSA04012 - TÍCH ĐA THỨC.cpp
+++ DSA04012 - TÍCH ĐA THỨC.cpp
@@ -15,15 +15,13 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int n, m;
         cin >> n >> m;
         vector<int> a(n), b(m);
@@ -32,10 +30,8 @@ int main()
         for (int &x : b)
             cin >> x;
         map<int, int> mp;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 mp[i + j] += a[i] * b[j];
             }
         }
