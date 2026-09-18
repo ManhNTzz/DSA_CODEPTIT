55cb3ce98924d366e4f9b2c613dec291de4368d4
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-18 08:44:06 +0700

    Code DSA

--- DSA04007 - HỆ CƠ SỐ K.cpp
+++ DSA04007 - HỆ CƠ SỐ K.cpp
@@ -15,30 +15,25 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	int T;
 	cin >> T;
-	while (T--)
-	{
+	while (T--) {
 		int k;
 		string a, b;
 		cin >> k >> a >> b;
-		while (a.len < b.len)
-		{
+		while (a.len < b.len) {
 			a = "0" + a;
 		}
-		while (b.len < a.len)
-		{
+		while (b.len < a.len) {
 			b = "0" + b;
 		}
 		string s = "";
 		int nho = 0;
-		FOD(i, 0, a.len - 1)
-		{
+		FOD(i, 0, a.len - 1) {
 			int digit = (a[i] - '0') + (b[i] - '0') + nho;
 			s = char(digit % k + '0') + s;
 			nho = digit / k;
