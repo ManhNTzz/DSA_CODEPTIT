67abff28bea5f1a08c50102872ea7f642b5eff94
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-19 07:21:15 +0700

    Code DSA

--- DSA04025 - DÃY SỐ VÔ HẠN.cpp
+++ DSA04025 - DÃY SỐ VÔ HẠN.cpp
@@ -15,8 +15,7 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-ll mul(ll a, ll b)
-{
+ll mul(ll a, ll b) {
     if (b == 0)
         return 0;
     ll ans = mul(a, b / 2);
@@ -26,16 +25,12 @@ ll mul(ll a, ll b)
     return ans;
 }
 
-vector<vector<ll>> fibo(vector<vector<ll>> a, vector<vector<ll>> b)
-{
+vector<vector<ll>> fibo(vector<vector<ll>> a, vector<vector<ll>> b) {
     vector<vector<ll>> res(2, vector<ll>(2, 0));
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
             res[i][j] = 0;
-            for (int k = 0; k < 2; k++)
-            {
+            for (int k = 0; k < 2; k++) {
                 res[i][j] = (res[i][j] + mul(a[i][k], b[k][j])) % mod;
             }
         }
@@ -43,13 +38,11 @@ vector<vector<ll>> fibo(vector<vector<ll>> a, vector<vector<ll>> b)
     return res;
 }
 
-ll binPow(vector<vector<ll>> a, ll n)
-{
+ll binPow(vector<vector<ll>> a, ll n) {
     vector<vector<ll>> tmp = a;
     if (n == 1)
         return a[0][1];
-    while (n > 0)
-    {
+    while (n > 0) {
         if (n % 2 == 1)
             tmp = fibo(tmp, a);
         n /= 2;
@@ -58,12 +51,10 @@ ll binPow(vector<vector<ll>> a, ll n)
     return tmp[0][1];
 }
 
-int main()
-{
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         ll n;
         cin >> n;
         vector<vector<ll>> a = {{1, 1}, {1, 0}};
