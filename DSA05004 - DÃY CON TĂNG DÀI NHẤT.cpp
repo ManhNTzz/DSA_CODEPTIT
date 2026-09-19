163905d378b88785cf2a71b4f0d46fe1e6822194
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-20 06:39:55 +0700

    Code DSA

--- DSA05004 - DÃY CON TĂNG DÀI NHẤT.cpp
+++ DSA05004 - DÃY CON TĂNG DÀI NHẤT.cpp
@@ -13,19 +13,16 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
     vector<int> a(n);
     REP(i, n)
     cin >> a[i];
     vector<int> ans;
-    REP(i, n)
-    {
+    REP(i, n) {
         auto it = lower_bound(ans.begin(), ans.end(), a[i]);
-        if (it == ans.end())
-        {
+        if (it == ans.end()) {
             ans.push_back(a[i]);
         }
         else
