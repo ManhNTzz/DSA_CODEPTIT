608de393aba2fd1b53ff722a32771b275da7697d
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-20 06:38:25 +0700

    Code DSA

--- DSA04030 - SỐ TRIBONACCI.cpp
+++ DSA04030 - SỐ TRIBONACCI.cpp
@@ -17,8 +17,7 @@ using namespace std;
 ll MOD = 1e15 + 7;
 ll M[5] = {1, 3, 6, 12, 23};
 
-struct matran
-{
+struct matran {
     ll f[3][3] = {
         {1, 1, 1},
         {1, 0, 0},
@@ -26,8 +25,7 @@ struct matran
     };
 };
 
-ll Mul(ll a, ll b)
-{
+ll Mul(ll a, ll b) {
     if (b <= 1)
         return a % MOD * b;
     ll tmp = Mul(a, b / 2);
@@ -37,16 +35,12 @@ ll Mul(ll a, ll b)
         return tmp % MOD + tmp % MOD + a % MOD;
 }
 
-matran operator*(matran A, matran B)
-{
+matran operator*(matran A, matran B) {
     matran C;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             C.f[i][j] = 0;
-            for (int k = 0; k < 3; k++)
-            {
+            for (int k = 0; k < 3; k++) {
                 C.f[i][j] = (C.f[i][j] % MOD + Mul(A.f[i][k], B.f[k][j]) % MOD) % MOD;
             }
         }
@@ -54,8 +48,7 @@ matran operator*(matran A, matran B)
     return C;
 }
 
-matran Pow(matran X, ll k)
-{
+matran Pow(matran X, ll k) {
     if (k == 1)
         return X;
     matran tmp = Pow(X, k / 2);
@@ -65,8 +58,7 @@ matran Pow(matran X, ll k)
         return tmp * tmp * X;
 }
 
-ll Tribonacci(ll n)
-{
+ll Tribonacci(ll n) {
     if (n <= 3)
         return M[n - 1];
     matran X;
@@ -74,12 +66,10 @@ ll Tribonacci(ll n)
     return S.f[1][0] - 1;
 }
 
-int main()
-{
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         ll n;
         cin >> n;
         cout << Tribonacci(n) << endl;
