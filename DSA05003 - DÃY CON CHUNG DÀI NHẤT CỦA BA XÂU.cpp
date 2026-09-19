afcf964390277345a258072742680ecbb6754623
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-20 06:39:34 +0700

    Code DSA

--- DSA05003 - DÃY CON CHUNG DÀI NHẤT CỦA BA XÂU.cpp
+++ DSA05003 - DÃY CON CHUNG DÀI NHẤT CỦA BA XÂU.cpp
@@ -14,26 +14,21 @@ const long long mod = 1000000007;
 // Cre by ManhNTzz
 
 int f[101][101][101];
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	int T;
 	cin >> T;
-	while (T--)
-	{
+	while (T--) {
 		int n, m, k, res = 0;
 		cin >> n >> m >> k;
 		string x, y, z;
 		cin >> x >> y >> z;
 		memset(f, 0, sizeof(f));
-		for (int i = 1; i <= n; ++i)
-		{
-			for (int j = 1; j <= m; ++j)
-			{
-				for (int l = 1; l <= k; ++l)
-				{
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				for (int l = 1; l <= k; ++l) {
 					if (x[i - 1] == y[j - 1] && x[i - 1] == z[l - 1])
 						f[i][j][l] = f[i - 1][j - 1][l - 1] + 1;
 					else
