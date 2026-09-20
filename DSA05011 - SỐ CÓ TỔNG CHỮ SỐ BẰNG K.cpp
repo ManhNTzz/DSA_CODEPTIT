bdef485bc80e7e021549ce11dda4fb316c003739
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-21 06:57:26 +0700

    Code DSA

--- DSA05011 - SỐ CÓ TỔNG CHỮ SỐ BẰNG K.cpp
+++ DSA05011 - SỐ CÓ TỔNG CHỮ SỐ BẰNG K.cpp
@@ -15,12 +15,10 @@ const long long MOD = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
     int T;
     cin >> T;
-    while (T--)
-    {
+    while (T--) {
         int n, k;
         cin >> n >> k;
         vector<vector<ll>> dp(n + 1, vector<ll>(k + 1, 0));
@@ -28,12 +26,9 @@ int main()
         for (int i = 1; i <= 9; i++)
             dp[1][i] = 1;
 
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 0; j <= 9; j++)
-            {
-                for (int l = j; l <= k; l++)
-                {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= 9; j++) {
+                for (int l = j; l <= k; l++) {
                     dp[i][l] = (dp[i][l] + dp[i - 1][l - j]) % MOD;
                 }
             }
