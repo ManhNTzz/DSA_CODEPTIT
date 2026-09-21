8c0e58dbda8d78f781c8731cf597c7d34af52c2e
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-22 06:21:46 +0700

    Code DSA

--- DSA05019 - HÌNH VUÔNG LỚN NHẤT.cpp
+++ DSA05019 - HÌNH VUÔNG LỚN NHẤT.cpp
@@ -12,33 +12,26 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int n, m;
         cin >> n >> m;
         vector<vector<int>> a(n, vector<int>(m));
         int ans = 0;
-        for (auto &v : a)
-        {
-            for (int &x : v)
-            {
+        for (auto &v : a) {
+            for (int &x : v) {
                 cin >> x;
                 ans = max(ans, x);
             }
         }
         vector<vector<int>> dp = a;
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = 1; j < m; j++)
-            {
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
                 if (!a[i][j])
                     dp[i][j] = 0;
-                else
-                {
+                else {
                     dp[i][j] = min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]}) + 1;
                 }
                 ans = max(ans, dp[i][j]);
