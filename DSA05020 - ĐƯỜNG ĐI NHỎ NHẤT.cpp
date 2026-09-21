063ceb9069f47d211718324ffd9dae5329c88eeb
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-22 06:22:17 +0700

    Code DSA
    Refactor main function and loop structure for clarity.

--- DSA05020 - ĐƯỜNG ĐI NHỎ NHẤT.cpp
+++ DSA05020 - ĐƯỜNG ĐI NHỎ NHẤT.cpp
@@ -12,25 +12,20 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int n, m;
         cin >> n >> m;
         vector<vector<int>> a(n + 1, vector<int>(m + 1));
-        FOR(i, 1, n)
-        {
+        FOR(i, 1, n) {
             FOR(j, 1, m)
             cin >> a[i][j];
         }
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 1e9));
-        FOR(i, 1, n)
-        {
-            FOR(j, 1, m)
-            {
+        FOR(i, 1, n) {
+            FOR(j, 1, m) {
                 if (i == 1 && j == 1)
                     dp[i][j] = a[i][j];
                 else
