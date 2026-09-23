a69c449dbac96922cd11b430e9896ca92d43b496
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-24 06:58:45 +0700

    Code DSA

--- DSA05031 - TỔNG BÌNH PHƯƠNG.cpp
+++ DSA05031 - TỔNG BÌNH PHƯƠNG.cpp
@@ -12,20 +12,16 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int n;
         cin >> n;
         vector<int> dp(n + 1, 0);
-        for (int i = 1; i <= n; i++)
-        {
+        for (int i = 1; i <= n; i++) {
             dp[i] = i;
-            for (int j = 1; j <= sqrt(i); j++)
-            {
+            for (int j = 1; j <= sqrt(i); j++) {
                 dp[i] = min(dp[i], dp[i - j * j] + 1);
             }
         }
