ea44091272ba3869f8ba43fdce5fa6d0e1840364
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-23 08:38:38 +0700

    Code DSA

--- DSA05029 - GIẢI MÃ.cpp
+++ DSA05029 - GIẢI MÃ.cpp
@@ -12,24 +12,20 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         string s;
         cin >> s;
-        if (s[0] == '0')
-        {
+        if (s[0] == '0') {
             cout << 0 << endl;
             continue;
         }
         int dp[1005] = {};
         dp[0] = dp[1] = 1;
 
-        for (int i = 2; i <= s.size(); i++)
-        {
+        for (int i = 2; i <= s.size(); i++) {
             if (s[i - 1] > '0')
                 dp[i] = dp[i - 1];
             if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))
