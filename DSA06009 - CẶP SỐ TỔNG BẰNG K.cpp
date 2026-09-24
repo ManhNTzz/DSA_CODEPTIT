64d4cc14a732c0cc3f4e78e46923daa4683d369b
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-25 06:36:41 +0700

    Code DSA

--- DSA06009 - CẶP SỐ TỔNG BẰNG K.cpp
+++ DSA06009 - CẶP SỐ TỔNG BẰNG K.cpp
@@ -15,15 +15,13 @@ const long long mod = 1000000007;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     int T;
     cin >> T;
-    while (T--)
-    {
+    while (T--) {
         int n, k;
         cin >> n >> k;
         int a[n];
@@ -31,8 +29,7 @@ int main()
         cin >> a[i];
         int ans = 0;
         REP(i, n - 1)
-        FOR(j, i + 1, n - 1)
-        {
+        FOR(j, i + 1, n - 1) {
             if (a[i] + a[j] == k)
                 ans++;
         }
