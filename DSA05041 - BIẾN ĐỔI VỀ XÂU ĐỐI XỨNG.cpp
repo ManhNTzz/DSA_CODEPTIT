16dac4e4d6e8a34b43a9bcbd728ec42a54f09342
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-24 07:01:47 +0700

    Code DSA

--- DSA05041 - BIẾN ĐỔI VỀ XÂU ĐỐI XỨNG.cpp
+++ DSA05041 - BIẾN ĐỔI VỀ XÂU ĐỐI XỨNG.cpp
@@ -15,15 +15,13 @@ const long long mod = 1000000007;
 
 bool f[1005][1005];
 int n;
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	int T;
 	cin >> T;
-	while (T--)
-	{
+	while (T--) {
 		string s;
 		cin >> s;
 		string t = s;
@@ -32,10 +30,8 @@ int main()
 		int n = s.size();
 		vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
 
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = 1; j <= n; j++)
-			{
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
 				if (s[i - 1] == t[j - 1])
 					dp[i][j] = dp[i - 1][j - 1] + 1;
 				else
